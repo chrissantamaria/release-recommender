12bebb916824cc37976e76ca580f33f45f299ded
Chris Santamaria <chris@santamaria.me>
2020-11-08 22:53:34 -0500

    Simplify rewrites syntax

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  async rewrites() {
-    return process.env.NODE_ENV === 'development'
+  rewrites: () =>
+    process.env.NODE_ENV === 'development'
       ? [
           {
             source: '/api/:path*',
             destination: 'http://localhost:8080/api/:path*',
           },
         ]
-      : [];
-  },
+      : [],
 };
