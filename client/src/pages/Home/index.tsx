aee8514cdbb4e043ffa555fcfe40d0c87c55b3ca
Chris Santamaria <chris@santamaria.me>
2020-11-15 15:59:26 -0500

    Add header text to home

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -3,9 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import usePlaylists from '../../utils/usePlaylists';
 import PlaylistPreview from './PlaylistPreview';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  container: {
+  header: {
+    margin: '1rem 0',
+  },
+  grid: {
     display: 'flex',
     flexWrap: 'wrap',
   },
@@ -21,14 +25,25 @@ const Home = () => {
   if (!playlists) return null;
 
   return (
-    <div className={styles.container}>
-      {playlists.map((playlist) => (
-        <PlaylistPreview
-          key={playlist.id}
-          className={styles.preview}
-          {...playlist}
-        />
-      ))}
+    <div>
+      <div className={styles.header}>
+        <Typography variant="h4" gutterBottom>
+          queue up!
+        </Typography>
+        <Typography variant="h6">
+          choose as many of your playlists as you'd like.
+        </Typography>
+        <Typography variant="h6">more songs = more recommendations</Typography>
+      </div>
+      <div className={styles.grid}>
+        {playlists.map((playlist) => (
+          <PlaylistPreview
+            key={playlist.id}
+            className={styles.preview}
+            {...playlist}
+          />
+        ))}
+      </div>
     </div>
   );
 };
