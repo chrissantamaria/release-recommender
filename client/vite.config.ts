d187a1463ddfca212c5d502448e2642c7b61458a
Chris Santamaria <chris@santamaria.me>
2021-03-28 10:35:03 -0400

    Minify HTML in build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { minifyHtml } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    minifyHtml({
+      // http://perfectionkills.com/experimenting-with-html-minifier/#remove_attribute_quotes
+      removeAttributeQuotes: false,
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
