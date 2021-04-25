c01b37052b45a6661cf0f202dbf4dcbe2e63f381
Chris Santamaria <chris@santamaria.me>
2021-04-25 16:42:52 -0400

    Bump Vite to fix module alias bug

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
-import path from 'path';
 import type { UserConfigExport } from 'vite';
 import macrosPlugin from 'vite-plugin-babel-macros';
 import preact from '@preact/preset-vite';
-// import { getAliases } from 'vite-aliases';
+import { getAliases } from 'vite-aliases';
 import { minifyHtml } from 'vite-plugin-html';
 import legacy from '@vitejs/plugin-legacy';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -10,17 +9,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 const config: UserConfigExport = {
   resolve: {
-    // vite currently can't handle merging array-based aliases
-    // (from vite-aliases) with object-based aliases (from @preact/preset-vite),
-    // for now just manually creating object-based aliases.
-    // https://github.com/vitejs/vite/pull/2847
-    alias: {
-      '@components': path.resolve('./src/components'),
-      '@images': path.resolve('./src/images'),
-      '@pages': path.resolve('./src/pages'),
-      '@store': path.resolve('./src/store'),
-      '@utils': path.resolve('./src/utils'),
-    },
+    alias: getAliases(),
   },
   plugins: [
     preact(),
